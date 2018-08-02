05d231069f948f775d132392ae26978784ee3cae
James Pirruccello <jamesp@broadinstitute.org>
2018-08-02 15:05:00 -0400

    Undoes last commit

--- detecreaddatatype.go
+++ detecreaddatatype.go
@@ -40,6 +40,9 @@ func DetectDataType(r io.ReadSeeker) (DataType, error) {
 		return DataTypeInvalid, err
 	}
 
+	// Reset your original reader
+	defer r.Seek(0, 0)
+
 	// Match known signatures
 	for dt, sig := range byteCodeSigs {
 		for position := range sig {
@@ -59,7 +62,6 @@ func MaybeDecompressReadCloserFromFile(f *os.File) (io.ReadCloser, error) {
 	if err != nil {
 		return nil, err
 	}
-	f.Seek(0, 0)
 
 	switch dt {
 	case DataTypeGzip:
