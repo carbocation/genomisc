10d47e8893e268347a58544baa519a9152a76354
James Pirruccello <jamesp@broadinstitute.org>
2019-02-04 17:11:22 -0500

    Parallelized batcher.

--- cmd/batcher/main.go
+++ cmd/batcher/main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"strings"
 
@@ -66,6 +67,28 @@ func main() {
 			log.Fatalln(err)
 		}
 	}
+
+	// Manage concurrency
+	concurrency := 4 * runtime.NumCPU()
+	semaphore := make(chan struct{}, concurrency)
+	results := make(chan string, concurrency)
+	doneListening := make(chan struct{})
+	go func() {
+		defer func() { doneListening <- struct{}{} }()
+		// Serialize results so you don't dump text haphazardly into os.Stdout
+		// (which is not goroutine safe).
+		for {
+			select {
+			case res, ok := <-results:
+				if !ok {
+					return
+				}
+
+				fmt.Println(res)
+			}
+		}
+
+	}()
 
 	// Setup the temp folder
 	tempSubfolder := filepath.Join(tmpPath, RandOrthoglyphs(15))
@@ -150,92 +173,106 @@ func main() {
 	}
 
 	for zf, df := range hierarchy {
+		// Will block after `concurrency` simultaneous goroutines are running
+		semaphore <- struct{}{}
+
 		// Dicom list is constructed; now iterate over the zip files
+		go func(zf string, df map[string]struct{}) {
 
-		rc, err := zip.OpenReader(filepath.Join(path, zf))
-		if err != nil {
-			log.Fatalln(err)
-		}
+			// Be sure to permit unblocking once we finish
+			defer func() { <-semaphore }()
 
-		toFind := len(df)
-		for _, v := range rc.File {
-			// Iterate over all of the dicoms in the zip
-			if toFind == 0 {
-				break
-			}
-
-			if _, exists := df[v.Name]; !exists {
-				continue
-			}
-
-			dicomReader, err := v.Open()
+			rc, err := zip.OpenReader(filepath.Join(path, zf))
 			if err != nil {
 				log.Fatalln(err)
 			}
 
-			outputFileName := v.Name
+			toFind := len(df)
+			for _, v := range rc.File {
+				// Iterate over all of the dicoms in the zip
+				if toFind == 0 {
+					break
+				}
 
-			if makeJpeg {
-				outputFileName += ".jpg"
+				if _, exists := df[v.Name]; !exists {
+					continue
+				}
 
-				images, err := DicomToImages(dicomReader)
+				dicomReader, err := v.Open()
 				if err != nil {
 					log.Fatalln(err)
 				}
 
-				if len(images) != 1 {
-					log.Fatalf("Expected 1 jpeg per dicom, found %d in %s => %s\n", len(images), zf, v.Name)
+				outputFileName := v.Name
+
+				if makeJpeg {
+					outputFileName += ".jpg"
+
+					images, err := DicomToImages(dicomReader)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					if len(images) != 1 {
+						log.Fatalf("Expected 1 jpeg per dicom, found %d in %s => %s\n", len(images), zf, v.Name)
+					}
+
+					// Moved this inside the check. Unclear to me why I had this
+					// outside the makeJpg check.
+					extractedPath := filepath.Join(tempSubfolder, outputFileName)
+					extractedFile, err := os.OpenFile(extractedPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, v.Mode())
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					if err := jpeg.Encode(extractedFile, images[0], &jpeg.Options{Quality: 100}); err != nil {
+						log.Fatalln(err)
+					}
+				} else if makeImageFromOverlay {
+					outputFileName += ".overlay.png"
+					pngs, err := bulkprocess.DicomToOverlayImage(dicomReader, overlayOpts)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					if len(pngs) != 1 {
+						log.Fatalf("Expected 1 overlay per dicom, found %d in %s => %s\n", len(pngs), zf, v.Name)
+					}
+
+					extractedPath := filepath.Join(tempSubfolder, outputFileName)
+					extractedFile, err := os.OpenFile(extractedPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, v.Mode())
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					err = png.Encode(extractedFile, pngs[0])
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					// By emitting the output folder, we facilitate making this into
+					// a pipe-able tool.
+					results <- fmt.Sprint(extractedPath)
 				}
 
-				// Moved this inside the check. Unclear to me why I had this
-				// outside the makeJpg check.
-				extractedPath := filepath.Join(tempSubfolder, outputFileName)
-				extractedFile, err := os.OpenFile(extractedPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, v.Mode())
-				if err != nil {
-					log.Fatalln(err)
-				}
+				dicomReader.Close()
 
-				if err := jpeg.Encode(extractedFile, images[0], &jpeg.Options{Quality: 100}); err != nil {
-					log.Fatalln(err)
-				}
-
-				// fmt.Println(extractedPath)
+				toFind--
 			}
 
-			if makeImageFromOverlay {
-				outputFileName += ".overlay.png"
-				pngs, err := bulkprocess.DicomToOverlayImage(dicomReader, overlayOpts)
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				if len(pngs) != 1 {
-					log.Fatalf("Expected 1 overlay per dicom, found %d in %s => %s\n", len(pngs), zf, v.Name)
-				}
-
-				extractedPath := filepath.Join(tempSubfolder, outputFileName)
-				extractedFile, err := os.OpenFile(extractedPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, v.Mode())
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				err = png.Encode(extractedFile, pngs[0])
-				if err != nil {
-					log.Fatalln(err)
-				}
-
-				// By emitting the output folder, we facilitate making this into
-				// a pipe-able tool.
-				fmt.Println(extractedPath)
-			}
-
-			dicomReader.Close()
-
-			toFind--
-		}
-
-		rc.Close()
+			rc.Close()
+		}(zf, df)
 	}
+
+	// Make sure we finish all the reads before we exit, otherwise we'll lose
+	// the last `concurrency` lines.
+	for i := 0; i < cap(semaphore); i++ {
+		semaphore <- struct{}{}
+	}
+
+	// Close the results channel and make sure we are done listening
+	close(results)
+	<-doneListening
 }
 
 func AppendOpenFile(file *os.File, line []string, delimiter rune) error {
