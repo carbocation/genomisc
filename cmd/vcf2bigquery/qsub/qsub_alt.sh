faadc1dd6727b347e728b2dd6f1ec07319e4bdd5
James Pirruccello <james@carbocation.com>
2019-04-03 12:02:21 -0400

    Add submission script for alt and missing.

---
+++ cmd/vcf2bigquery/qsub/qsub_alt.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/hptmp/exome/linearize/altonly/errors/
+
+# Chunksize 500 => 5760 chunks
+
+qsub \
+  -cwd \
+  -sync ry \
+  -t 1-5760:1 \
+  -l h_vmem=2g \
+  -binding linear:2 -pe smp 2 \
+  -l h_rt="02:00:00" \
+  -v CHUNK_SIZE=500 \
+  -o ~/hptmp/exome/linearize/altonly/linear.\$TASK_ID.tsv.gz \
+  -e ~/hptmp/exome/linearize/altonly/errors/error.\$TASK_ID \
+  vcf2bigquery.sh
