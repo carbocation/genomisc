2d69e355f55f0797a806306649157fd8b24e0b15
James Pirruccello <jamesp@broadinstitute.org>
2019-06-11 21:19:55 -0400

    Experimenting with cutpoints for max. I don't think this is little endian by the time I'm touching it.

--- cmd/critic/web/extractdicom.go
+++ cmd/critic/web/extractdicom.go
@@ -9,20 +9,28 @@ import (
 	"io/ioutil"
 	"log"
 	"math"
+	"strconv"
 
 	"github.com/suyashkumar/dicom"
 	"github.com/suyashkumar/dicom/dicomtag"
 	"github.com/suyashkumar/dicom/element"
 )
 
+// https://stackoverflow.com/a/16641340/199475
+
 // ExtractDicom constructs a native go Image type from the dicom image with the
 // given name in the given zip file.
 func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
+	var err error
+
 	rc, err := zip.OpenReader(zipPath)
 	if err != nil {
 		return nil, err
 	}
 	defer rc.Close()
+
+	// We will read our 2-byte (16-bit) little endian values into this buffer
+	pxBuf := make([]byte, 2)
 
 	for _, v := range rc.File {
 		// Iterate over all of the dicoms in the zip til we find the one with
@@ -64,8 +72,8 @@ func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
 			return nil, fmt.Errorf("Error reading %s: %v", zipPath, err)
 		}
 
-		var bitsAllocated, bitsStored, highBit uint16
-		_, _, _ = bitsAllocated, bitsStored, highBit
+		var bitsAllocated, bitsStored, highBit, windowCenter, windowWidth, windowCenterBE, windowWidthBE uint16
+		_, _, _, _, _, _, _ = bitsAllocated, bitsStored, highBit, windowCenter, windowWidth, windowCenterBE, windowWidthBE
 
 		for _, elem := range parsedData.Elements {
 
@@ -81,6 +89,29 @@ func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
 			} else if elem.Tag == dicomtag.HighBit {
 				log.Printf("HighBit: %+v %T\n", elem.Value, elem.Value[0])
 				highBit = elem.Value[0].(uint16)
+			} else if elem.Tag == dicomtag.WindowCenter {
+				log.Printf("WindowCenter: %+v %T\n", elem.Value, elem.Value[0])
+				windowCenter64, err := strconv.ParseUint(elem.Value[0].(string), 10, 16)
+				if err != nil {
+					return nil, err
+				}
+				windowCenter = uint16(windowCenter64)
+
+				// Get the big-endian representation
+				binary.LittleEndian.PutUint16(pxBuf, windowCenter)
+				windowCenterBE = binary.BigEndian.Uint16(pxBuf)
+
+			} else if elem.Tag == dicomtag.WindowWidth {
+				log.Printf("WindowWidth: %+v %T\n", elem.Value, elem.Value[0])
+				windowWidth64, err := strconv.ParseUint(elem.Value[0].(string), 10, 16)
+				if err != nil {
+					return nil, err
+				}
+				windowWidth = uint16(windowWidth64)
+
+				// Get the big-endian representation
+				binary.LittleEndian.PutUint16(pxBuf, windowWidth)
+				windowWidthBE = binary.BigEndian.Uint16(pxBuf)
 			}
 
 			if elem.Tag == dicomtag.PixelRepresentation {
@@ -103,24 +134,34 @@ func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
 				log.Printf("SmallestImagePixelValue: %+v %T\n", elem.Value, elem.Value[0])
 			} else if elem.Tag == dicomtag.LargestImagePixelValue {
 				log.Printf("LargestImagePixelValue: %+v %T\n", elem.Value, elem.Value[0])
-			} else if elem.Tag == dicomtag.WindowCenter {
-				log.Printf("WindowCenter: %+v %T\n", elem.Value, elem.Value[0])
-			} else if elem.Tag == dicomtag.WindowWidth {
-				log.Printf("WindowWidth: %+v %T\n", elem.Value, elem.Value[0])
 			} else if elem.Tag == dicomtag.VOILUTFunction {
 				log.Printf("VOILUTFunction: %+v %T\n", elem.Value, elem.Value[0])
 			}
 
 			if elem.Tag == dicomtag.PixelData {
+				// https://stackoverflow.com/a/8765366/199475
+				var lowestVisibleValue, highestVisibleValue uint16
+				if windowWidth > 0 {
+					lowestVisibleValue = uint16(float64(windowCenter) - float64(windowWidth)/float64(2))
+					highestVisibleValue = uint16(float64(windowCenter) + float64(windowWidth)/float64(2))
+
+					log.Println("LowestVisibleLE", lowestVisibleValue, "HighestVisibleLE", highestVisibleValue)
+
+					binary.LittleEndian.PutUint16(pxBuf, lowestVisibleValue)
+					lowestVisibleValue = binary.BigEndian.Uint16(pxBuf)
+
+					binary.LittleEndian.PutUint16(pxBuf, highestVisibleValue)
+					highestVisibleValue = binary.BigEndian.Uint16(pxBuf)
+
+					log.Println("LowestVisibleLETx", lowestVisibleValue, "HighestVisibleLETx", highestVisibleValue)
+				}
+
 				data := elem.Value[0].(element.PixelDataInfo)
 
 				for _, frame := range data.Frames {
 					if frame.IsEncapsulated {
 						return nil, fmt.Errorf("Frame is encapsulated, which we did not expect")
 					}
-
-					// We will read our 2-byte (16-bit) little endian values into this buffer
-					pxBuf := make([]byte, 2)
 
 					// For normalization
 					var lowest uint16 = math.MaxUint16
@@ -130,45 +171,70 @@ func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
 					var highestLE uint16
 					for j := 0; j < len(frame.NativeData.Data); j++ {
 						leVal := uint16(frame.NativeData.Data[j][0])
+
+						if leVal < lowestLE {
+							lowestLE = leVal
+						}
+						if leVal > highestLE {
+							highestLE = leVal
+						}
+
+						// if leVal < lowestVisibleValue {
+						// 	leVal = lowestVisibleValue
+						// } else if leVal > highestVisibleValue {
+						// 	leVal = highestVisibleValue
+						// }
+
 						binary.LittleEndian.PutUint16(pxBuf, leVal)
 						px := binary.BigEndian.Uint16(pxBuf)
 
 						if px < lowest {
-							// lowest = px
+							lowest = px
 							// lowestLE = leVal
 						}
 
 						if px > highest {
-							// highest = px
-							// highestLE = leVal
-						}
-
-						if leVal < lowestLE {
-							lowest = px
-							lowestLE = leVal
-						}
-						if leVal > highestLE {
-							// highestLE = leVal
 							highest = px
-							highestLE = leVal
+							// highestLE = leVal
 						}
 					}
 
 					log.Println("Lowest", lowest, "Highest", highest)
 					log.Println("Lowest little endian", lowestLE, "Highest little endian", highestLE)
+					log.Println("Window Center", windowCenter, "Window Width", windowWidth)
+					log.Println("WindowCenterBE", windowCenterBE, "windowWidthBE", windowWidthBE)
 
 					i := image.NewGray16(image.Rect(0, 0, frame.NativeData.Cols, frame.NativeData.Rows))
 					// pxBuf = make([]byte, 2) // 2 bytes == 16 bits
 					for j := 0; j < len(frame.NativeData.Data); j++ {
+						leVal := uint16(frame.NativeData.Data[j][0])
 
-						// The UK Biobank images I've seen so far are 16-bit
-						binary.LittleEndian.PutUint16(pxBuf, uint16(frame.NativeData.Data[j][0]))
+						// if leVal < lowestVisibleValue {
+						// 	leVal = lowestVisibleValue
+						// } else if leVal > highestVisibleValue {
+						// 	leVal = highestVisibleValue
+						// }
 
+						binary.LittleEndian.PutUint16(pxBuf, leVal)
 						px := binary.BigEndian.Uint16(pxBuf)
+						_ = px
+
+						if px > highestVisibleValue {
+							px = 1<<16 - 1
+						}
+
+						if leVal > 220 && leVal < 270 {
+							px = 1<<16 - 1
+						}
+
+						// ((byte[0] & 0x0f) << 8) | byte[1];
+						// px = (uint16(pxBuf[1]&0x0f) << 8) | uint16(pxBuf[0])
+
+						i.SetGray16(j%frame.NativeData.Cols, j/frame.NativeData.Rows, color.Gray16{Y: uint16(px)})
 
 						// i.SetGray16(j%frame.NativeData.Cols, j/frame.NativeData.Rows, color.Gray16{Y: uint16(px << (bitsAllocated - bitsStored))})
 						// i.SetGray16(j%frame.NativeData.Cols, j/frame.NativeData.Rows, color.Gray16{Y: uint16(px)})
-						i.SetGray16(j%frame.NativeData.Cols, j/frame.NativeData.Rows, color.Gray16{Y: uint16(float64(1<<16) * math.Min(1.0, (float64(px)-float64(lowest))/(float64(highest)-float64(lowest))))})
+						// i.SetGray16(j%frame.NativeData.Cols, j/frame.NativeData.Rows, color.Gray16{Y: uint16(float64(1<<16) * math.Min(1.0, (float64(px)-float64(lowest))/(float64(highest)-float64(lowest))))})
 					}
 
 					return i, nil
@@ -181,3 +247,8 @@ func ExtractDicom(zipPath, dicomName string) (image.Image, error) {
 
 	return nil, fmt.Errorf("Did not find the requested Dicom %s in the Zip %s", dicomName, zipPath)
 }
+
+// func ApplyWindowScaling(intensity uint16, center, width float64) {
+// 	x := float64(intensity)
+// 	if x < center - 0.5
+// }
