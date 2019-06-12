e3f40d3d78cb9c9ace37777ee7e96f196ea1b948
James Pirruccello <james@carbocation.com>
2019-06-12 09:54:31 -0400

    Add SliceThickness. This is basically bogus and invalid, but it's better than the current plan which is zero approximation.

--- cmd/manifester/main.go
+++ cmd/manifester/main.go
@@ -28,7 +28,7 @@ func main() {
 	}
 
 	// Read each zip (names are significant)
-	fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%v\t%v\n",
+	fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%v\t%v\t%v\n",
 		"sample_id",
 		"field_id",
 		"instance",
@@ -47,6 +47,7 @@ func main() {
 		"image_z",
 		"px_height_mm",
 		"px_width_mm",
+		"slice_thickness_mm",
 	)
 
 	concurrency := 4 * runtime.NumCPU()
@@ -123,10 +124,11 @@ func PrintCSVRow(row bulkprocess.DicomOutput, results chan<- string) error {
 		overlayText = "HasOverlay"
 	}
 
-	results <- fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%.8f\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f",
+	results <- fmt.Sprintf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%.8f\t%d\t%d\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f",
 		row.SampleID, row.FieldID, row.Instance, row.Index, row.ZipFile,
 		row.Dicom.Filename, row.Dicom.SeriesDescription, studyDate.Format("2006-01-02"),
 		row.DicomMeta.InstanceNumber, overlayText, row.DicomMeta.OverlayFraction, row.DicomMeta.OverlayRows, row.DicomMeta.OverlayCols,
-		row.DicomMeta.PatientX, row.DicomMeta.PatientY, row.DicomMeta.PatientZ, row.DicomMeta.PixelHeightMM, row.DicomMeta.PixelWidthMM)
+		row.DicomMeta.PatientX, row.DicomMeta.PatientY, row.DicomMeta.PatientZ, row.DicomMeta.PixelHeightMM, row.DicomMeta.PixelWidthMM,
+		row.DicomMeta.SliceThicknessMM)
 	return nil
 }
