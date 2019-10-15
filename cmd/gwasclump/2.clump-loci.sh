831b4b4a444ca4ccc96db9e82f10ad4d71adb87b
James Pirruccello <jamesp@broadinstitute.org>
2019-10-15 18:13:19 -0400

    A set of demo scripts to identify distinct GWAS loci from BOLT-LMM output. Note: this only looks at CHR:POS and doesn't handle allelic heterogeneity. It's useful for "counting loci" from a GWAS.

---
+++ cmd/gwasclump/2.clump-loci.sh
@@ -0,0 +1,4 @@
+Rscript \
+    --vanilla clump-by-p-and-distance.r \
+    --file "genomewide.loci" --pvalue=5e-8 \
+    --locusRadius 500000
--- cmd/gwasclump/clump-by-p-and-distance.r
+++ cmd/gwasclump/clump-by-p-and-distance.r
@@ -1,25 +1,21 @@
-list.of.packages <- c("ggplot2", "Hmisc", "data.table", "speedglm", "data.table", "qqman", "CMplot", "optparse")
+list.of.packages <- c("data.table", "optparse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages,repos = "http://cran.us.r-project.org")
 
 library("optparse")
-library("speedglm")
-library("ggplot2")
 library("data.table")
-library("qqman")
-library("CMplot")
 
 option_list = list(
     make_option(c("-f", "--file"), type="character", default=NULL, help="BOLT output file"),
-    make_option("--pvalue", type="numeric", default=5e-8, help="P value threshold, below which loci are considered significant"),
+    make_option(c("-p", "--pvalue"), type="numeric", default=5e-8, help="P value threshold, below which loci are considered significant"),
     make_option(c("-r", "--locusRadius"), type="numeric", default=NULL, help="Number of bases around each lead SNP to exclude other potential lead SNPs")
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-print(opt$file)
 
-stop("OK")
+message("Options enabled:")
+message(paste(opt, collapse="\n"))
 
 snpDistanceCutoff <- function(pos, bases) {
     lower = pos-bases
@@ -51,10 +47,18 @@ pruneSNPs <- function(dat, flanking = 5e5, significance = 5e-8) {
     
     # Identify the window around the SNP
     dist = snpDistanceCutoff(best$BP, flanking)
+
+    # Destroy the neighboring SNPs
+    remaining <- subset(dat, CHR != best$CHR | BP < dist$lower | BP > dist$upper )
+
+    snpsDestroyed = nrow(dat) - nrow(remaining)
+
+    # Track that:
+    best$Neighbors = snpsDestroyed
     
     # Return the SNP and the remaining data with the 
     # flanking SNPs around the newest best SNP removed
-    list(best = best, remaining = subset(dat, CHR != best$CHR | BP < dist$lower | BP > dist$upper ))
+    list(best = best, remaining = remaining)
 }
 
 iteratively.prune <- function(df, flanking=1e6, significance=1e-7) {
@@ -80,48 +84,25 @@ iteratively.prune <- function(df, flanking=1e6, significance=1e-7) {
     all.best
 }
 
-P <- "5e-7"
-N <- "29041"
+dat <- fread(opt$file, showProgress=TRUE)
 
-input.file <- sprintf(
-    "gsutil cat gs://ukbb_v2/projects/jamesp/projects/gwas/lv20k/results/v42/corrected_extracted_%%s.bgen.stats.gz | gunzip -c | awk 'NR==1; $12>0 && $15>0 && $16<%s && ( ($7<0.5 && $8*$7*2*%s > 100) || ($7>0.5 && $8*(1.0-$7)*2*%s > 100) ) {print $0}'", 
-    P,
-    N,
-    N)
+pruned <- iteratively.prune(dat, significance = opt$pvalue, flanking=opt$locusRadius)
+N_Loci = nrow(pruned)
 
-input.file
-
-traits <- c("lvef", "lvedv", "lvesv", "sv", "lvedvi", "lvesvi", "svi")
-# traits <- c("svi")
-
-res <- list()
-i <- 1
-for(trait in traits) {
-    df <- fread(cmd=sprintf(input.file, trait))
-    
-    for(cutoff in c(5e-7, 5e-8, 5e-9, 5e-10)){
-        i <- i+1
-        
-        N_Loci = nrow(iteratively.prune(df, significance = cutoff))
-        if(is.null(N_Loci)) {
-            N_Loci = 0
-        } else if(!is.finite(N_Loci)) {
-            N_Loci = 0
-        }
-
-        res[[i]] <- data.frame(
-            Trait = trait,
-            Cutoff = cutoff,
-            N_Loci = N_Loci
-        )
-    }
+if(is.null(N_Loci)) {
+    N_Loci = 0
+} else if(!is.finite(N_Loci)) {
+    N_Loci = 0
 }
 
-do.call("rbind", res)
+res <- data.frame(
+    File = opt$file,
+    Cutoff = opt$pvalue,
+    N_Loci = N_Loci
+)
 
-results <- do.call("rbind", res)
+message(paste(res, collapse="\t"))
 
-wide.results <- reshape(results, idvar = "Trait", timevar = "Cutoff", direction = "wide")
-wide.results
+write.table(pruned, stdout(), sep = "\t", row.names = FALSE, quote = FALSE)
 
-addmargins(as.matrix(wide.results[,c(2,3,4,5)]))
+#pruned
