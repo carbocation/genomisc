62516bfff4daa87d93daf0e44e0f6601457152b0
James Pirruccello <james@carbocation.com>
2019-12-31 19:07:21 -0500

    Add a hex to nrgba function.

--- overlay/functions.go
+++ overlay/functions.go
@@ -39,20 +39,50 @@ func rgbaFromColorCode(colorCode string) (color.Color, error) {
 	}
 
 	// Parse each channel
-	r, err := strconv.ParseInt(colorCode[0:2], 16, 16)
+	r, err := strconv.ParseUint(colorCode[0:2], 16, 8)
 	if err != nil {
 		return nil, err
 	}
-	g, err := strconv.ParseInt(colorCode[2:4], 16, 16)
+	g, err := strconv.ParseUint(colorCode[2:4], 16, 8)
 	if err != nil {
 		return nil, err
 	}
-	b, err := strconv.ParseInt(colorCode[4:6], 16, 16)
+	b, err := strconv.ParseUint(colorCode[4:6], 16, 8)
 	if err != nil {
 		return nil, err
 	}
 
 	return color.RGBA{
+		R: uint8(r),
+		G: uint8(g),
+		B: uint8(b),
+		A: 255,
+	}, nil
+}
+
+func nrgbaFromColorCode(colorCode string) (color.Color, error) {
+	colorCode = strings.ReplaceAll(colorCode, "#", "")
+
+	// Special case the background
+	if len(colorCode) < 6 {
+		return color.RGBA{0, 0, 0, 0}, nil
+	}
+
+	// Parse each channel
+	r, err := strconv.ParseUint(colorCode[0:2], 16, 8)
+	if err != nil {
+		return nil, err
+	}
+	g, err := strconv.ParseUint(colorCode[2:4], 16, 8)
+	if err != nil {
+		return nil, err
+	}
+	b, err := strconv.ParseUint(colorCode[4:6], 16, 8)
+	if err != nil {
+		return nil, err
+	}
+
+	return color.NRGBA{
 		R: uint8(r),
 		G: uint8(g),
 		B: uint8(b),
