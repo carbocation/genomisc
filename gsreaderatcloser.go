15d92a94061cba63732bb4b975a8ddf87003519f
James Pirruccello <james@carbocation.com>
2020-08-12 00:09:41 -0400

    Identified a bug in gsreaderatcloser where, if the number of bytes requested wasn't read immediately, the Google Storage apparatus does not loop for you. So, I added a manual loop until we have read at least nBytes...

--- gsreaderatcloser.go
+++ gsreaderatcloser.go
@@ -22,7 +22,24 @@ func (o GSReaderAtCloser) ReadAt(p []byte, offset int64) (n int, err error) {
 	}
 	defer rdr.Close()
 
-	return rdr.Read(p)
+	// Apparently with Google Cloud Storage we get to manually handle the fact
+	// that the reader will not read as much as we request, but instead may read
+	// less than that. And so here, we manually check if that's the case; if so,
+	// we loop until we have read at least as much as we requested.
+	var nBytes int
+	for {
+		innerBytes, err := rdr.Read(p[nBytes:])
+		nBytes += innerBytes
+		if err != nil {
+			return nBytes, err
+		}
+
+		if rdr.Remain() <= 0 {
+			break
+		}
+	}
+
+	return nBytes, nil
 }
 
 // Satisfies io.Closer. If o.close is not set, this is a nop.
