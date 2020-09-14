e9f5ed5515fc272e81d73457ccaa8280e4c3e5fa
James Pirruccello <james@carbocation.com>
2020-09-13 23:22:57 -0400

    Adds additional methods for fetching images, including by fetching them from UK Biobank-style Zip files.

--- cmd/animategcp/makegif.go
+++ cmd/animategcp/makegif.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"image"
 	"image/color"
@@ -9,6 +10,7 @@ import (
 	"log"
 	"os"
 
+	"cloud.google.com/go/storage"
 	"github.com/carbocation/genomisc/ukbb/bulkprocess"
 	"github.com/carbocation/go-quantize/quantize"
 )
@@ -19,7 +21,7 @@ type gsData struct {
 }
 
 func makeOneGif(pngs []string, outName string, delay int) error {
-	outGif, err := MakeOneGIFFromPaths(pngs, delay)
+	outGif, err := MakeOneGIFFromPaths(pngs, delay, client)
 	if err != nil {
 		return err
 	}
@@ -35,15 +37,103 @@ func makeOneGif(pngs []string, outName string, delay int) error {
 	return gif.EncodeAll(f, outGif)
 }
 
-// MakeOneGIFFromPaths creates an animated gif from an ordered slice of paths -
-// which may be local or hosted in an accessible Google Storage location. (The
-// string for each png should be a fully specified path.)
-func MakeOneGIFFromPaths(pngs []string, delay int) (*gif.GIF, error) {
+func makeOneGifFromZip(dicomNames []string, zipPath string, outName string, delay int) error {
+	outGif, err := MakeOneGIFFromZIP(dicomNames, zipPath, delay, client)
+	if err != nil {
+		return err
+	}
+
+	// Save file
+	f, err := os.OpenFile(outName, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	return gif.EncodeAll(f, outGif)
+}
+
+func MakeOneGIFFromZIP(dicomNames []string, zipPath string, delay int, storageClient *storage.Client) (*gif.GIF, error) {
+	images, err := FetchImagesFromZIPByDICOMNames(dicomNames, zipPath, false, storageClient)
+	if err != nil {
+		return nil, err
+	}
+
+	outGif, err := MakeOneGIF(images, delay)
+	if err != nil {
+		return nil, err
+	}
+
+	return outGif, nil
+}
+
+// FetchImagesFromZIPByDICOMNames fetches images from DICOM files within a Zip
+// file. The Zip file may be local or on Google Storage.
+func FetchImagesFromZIPByDICOMNames(dicomNames []string, zipPath string, includeOverlay bool, storageClient *storage.Client) ([]image.Image, error) {
+
+	dicomNameMap := make(map[string]struct{})
+	for _, dicomName := range dicomNames {
+		dicomNameMap[dicomName] = struct{}{}
+	}
+
+	pngDats := make(map[string]image.Image)
+
+	// Read the zip file into memory still compressed - either from a local
+	// file, or from Google storage, depending on the prefix you provide.
+	readerAt, zipNBytes, err := bulkprocess.MaybeOpenFromGoogleStorage(zipPath, storageClient)
+	if err != nil {
+		return nil, err
+	}
+	defer readerAt.Close()
+
+	rc, err := zip.NewReader(readerAt, zipNBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate over the files within the Zip
+	for _, v := range rc.File {
+		if _, exists := dicomNameMap[v.Name]; !exists {
+			continue
+		}
+
+		// Read the desired DICOMs into images
+
+		dicomReader, err := v.Open()
+		if err != nil {
+			return nil, err
+		}
+		defer dicomReader.Close()
+
+		img, err := bulkprocess.ExtractDicomFromReader(dicomReader, includeOverlay)
+		if err != nil {
+			return nil, err
+		}
+		dicomReader.Close()
+
+		pngDats[v.Name] = img
+	}
+
+	// Make sure that the images are in the same sort order as the original
+	// input slice
+	out := make([]image.Image, 0, len(dicomNames))
+	for _, dicom := range dicomNames {
+		out = append(out, pngDats[dicom])
+	}
+
+	return out, nil
+}
+
+// MakeOneGIFFromPaths creates an animated gif from an ordered slice of paths to
+// image files - which may be local or hosted in an accessible Google Storage
+// location. (The string for each png should be a fully specified path.)
+func MakeOneGIFFromPaths(pngs []string, delay int, storageClient *storage.Client) (*gif.GIF, error) {
 	fetches := make(chan gsData)
 
 	for _, input := range pngs {
 		go func(input string) {
-			pngImage, err := bulkprocess.MaybeExtractImageFromGoogleStorage(input, client)
+			pngImage, err := bulkprocess.MaybeExtractImageFromGoogleStorage(input, storageClient)
 			if err != nil {
 				log.Println(err, "when operating on", input)
 			}
