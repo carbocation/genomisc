06c92cb06463e19947bce4f90eb3594d227245d6
James Pirruccello <james@carbocation.com>
2021-01-17 18:57:54 -0500

    Add option to create one entry per chr:pos.

--- cmd/enhanceratlas/enhancergene/main.go
+++ cmd/enhanceratlas/enhancergene/main.go
@@ -19,8 +19,10 @@ func main() {
 
 func run() error {
 	var filename string
+	var tall bool
 
 	flag.StringVar(&filename, "filename", "", "File containing EnhancerAtlas.org _EP_v2 style data, which is enhancer-gene data")
+	flag.BoolVar(&tall, "tall", false, "If true, creates one entry per chr-pos. If false, creates a summary entry indicating the pos start and stop positions.")
 	flag.Parse()
 
 	if filename == "" {
@@ -40,9 +42,13 @@ func run() error {
 		return err
 	}
 
-	fmt.Printf("tissue\tchr\tstart_grch37\tstop_grch37\tensembl_id\tgene_name\n")
+	if tall {
+		fmt.Printf("tissue\tchr\tpos_grch37\tensembl_id\tgene_name\tscore\n")
+	} else {
+		fmt.Printf("tissue\tchr\tstart_grch37\tstop_grch37\tensembl_id\tgene_name\tscore\n")
+	}
 
-	for _, row := range recs {
+	for i, row := range recs {
 		if len(row) != 2 {
 			continue
 		}
@@ -55,25 +61,40 @@ func run() error {
 			return err
 		}
 
-		parts := strings.Split(row[0], "_")
+		parts := strings.SplitN(row[0], "_", 2)
 		if x := len(parts); x != 2 {
-			return fmt.Errorf("Expected 2 parts when splitting by _ but found %d", x)
+			return fmt.Errorf("Line %d: Expected 2 parts when splitting by _ but found %d", i, x)
 		}
 
 		chpos := strings.Split(parts[0], ":")
 		startstop := strings.Split(chpos[1], "-")
 
+		start, err := strconv.ParseInt(startstop[0], 10, 64)
+		if err != nil {
+			return err
+		}
+
+		stop, err := strconv.ParseInt(startstop[1], 10, 64)
+		if err != nil {
+			return err
+		}
+
 		ep.Chromosome = strings.ReplaceAll(chpos[0], "chr", "")
-		ep.Start = startstop[0]
-		ep.Stop = startstop[1]
+		ep.Start = start
+		ep.Stop = stop
 		ep.EnhancerGeneInteractionScore = score
 
 		geneparts := strings.Split(parts[1], "$")
 
 		ep.EnsemblID = geneparts[0]
 		ep.GeneName = geneparts[1]
-
-		fmt.Printf("%s\t%s\t%s\t%s\t%s\t%s\n", ep.Tissue, ep.Chromosome, ep.Start, ep.Stop, ep.EnsemblID, ep.GeneName)
+		if tall {
+			for i := ep.Start; i <= ep.Stop; i++ {
+				fmt.Printf("%s\t%s\t%d\t%s\t%s\t%.3f\n", ep.Tissue, ep.Chromosome, i, ep.EnsemblID, ep.GeneName, ep.EnhancerGeneInteractionScore)
+			}
+		} else {
+			fmt.Printf("%s\t%s\t%d\t%d\t%s\t%s\t%.3f\n", ep.Tissue, ep.Chromosome, ep.Start, ep.Stop, ep.EnsemblID, ep.GeneName, ep.EnhancerGeneInteractionScore)
+		}
 	}
 
 	return nil
@@ -82,8 +103,8 @@ func run() error {
 type EP struct {
 	Tissue                       string
 	Chromosome                   string
-	Start                        string
-	Stop                         string
+	Start                        int64
+	Stop                         int64
 	EnsemblID                    string
 	GeneName                     string
 	EnhancerGeneInteractionScore float64
