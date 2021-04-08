f70a5ed3375608cf685af6f1be77dcc80c56dbc8
James Pirruccello <james@carbocation.com>
2021-04-08 09:03:19 -0400

    Add build scripts for static compilation including glibc.

---
+++ cmd/bgen2bigquery/build-linux.sh
@@ -0,0 +1,3 @@
+# Statically link, so that if CGO is used, it doesn't dynamically link to 
+# a GCC on the local system which may have a different version.
+GOOS=linux go build -ldflags="-extldflags=-static" -o bgen2bigquery.linux *.go
