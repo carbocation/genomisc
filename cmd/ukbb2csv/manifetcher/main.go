fabce58f133cd107f1167a98d7ac949478f2bed4
James Pirruccello <james@carbocation.com>
2021-09-06 15:14:37 -0400

    Update manifetcher to also work with non-bulk data.

--- cmd/ukbb2csv/manifetcher/main.go
+++ cmd/ukbb2csv/manifetcher/main.go
@@ -24,19 +24,23 @@ var bulkSuffixDel string
 var bulkSuffixAdd string
 var fileSuffixDel string
 var fileSuffixAdd string
-var bulkColName = "zip_file"
-var fileColName = "dicom_file"
+var nobulk bool
+var bulkColName string
+var fileColName string
 
 func main() {
 	var presetFlag string
 
 	flag.StringVar(&manifestPath, "manifest", "", "Path to manifest file. May be on Google Storage.")
+	flag.BoolVar(&nobulk, "nobulk", false, "If true, looks for raw image files in the --files path, skipping the bulk file step")
 	flag.StringVar(&filesPath, "files", "", "Folder path where bulk files reside. May be on Google Storage.")
 	flag.StringVar(&outputPath, "output", "", "Local path into which the extracted files will go.")
 	flag.StringVar(&bulkSuffixDel, "bs_del", "", "Suffix to delete from the manifest->bulk column.")
 	flag.StringVar(&bulkSuffixAdd, "bs_add", "", "Suffix to add to the manifest->bulk column.")
 	flag.StringVar(&fileSuffixDel, "fs_del", "", "Suffix to delete from the manifest->image file column.")
 	flag.StringVar(&fileSuffixAdd, "fs_add", "", "Suffix to add to the manifest->image file column.")
+	flag.StringVar(&bulkColName, "bulk_col_name", "zip_file", "Column name containing bulk file name (unless --nobulk is set)")
+	flag.StringVar(&fileColName, "image_col_name", "dicom_file", "Column name containing image file name")
 	flag.StringVar(&presetFlag, "preset", "", "If not empty, may be 'image' or 'overlay' to override the fs_* settings with defaults.")
 	flag.Parse()
 
@@ -107,11 +111,16 @@ func run() error {
 			return fmt.Errorf("header column '%s' not detected", fileColName)
 		}
 
-		if header.Zip == -1 {
+		if header.Zip == -1 && !nobulk {
 			return fmt.Errorf("header column '%s' not detected", bulkColName)
 		}
 
-		bulkFileName := FormatSuffix(cols[header.Zip], bulkSuffixDel, bulkSuffixAdd)
+		var bulkFileName string
+		if nobulk {
+			bulkFileName = ""
+		} else {
+			bulkFileName = FormatSuffix(cols[header.Zip], bulkSuffixDel, bulkSuffixAdd)
+		}
 		fileName := FormatSuffix(cols[header.Dicom], fileSuffixDel, fileSuffixAdd)
 
 		if i == 1 {
@@ -129,29 +138,45 @@ func run() error {
 			safeBulk := lastBulk
 
 			sem <- struct{}{}
-			go func(filesPath, bulkFileName string, fileNames map[string]struct{}, outputPath string) {
-				if err := DownloadFiles(filesPath, bulkFileName, fileNames, outputPath); err != nil {
-					log.Println(err)
-				}
-				<-sem
-			}(filesPath, safeBulk, safeMap, outputPath)
+			if !nobulk {
+				go func(filesPath, bulkFileName string, fileNames map[string]struct{}, outputPath string) {
+					if err := DownloadFiles(filesPath, bulkFileName, fileNames, outputPath); err != nil {
+						log.Println(err)
+					}
+					<-sem
+				}(filesPath, safeBulk, safeMap, outputPath)
+			}
 
 			// Now set the new image map and bulk file
 			lastBulk = bulkFileName
 			accumulatedImageNames = make(map[string]struct{})
+			accumulatedImageNames[fileName] = struct{}{}
+		} else if bulkFileName == lastBulk {
 			accumulatedImageNames[fileName] = struct{}{}
 		}
 	}
 
 	// We most likely have additional accumulated files that need to be processed
 	if len(accumulatedImageNames) > 0 {
-		sem <- struct{}{}
-		go func(filesPath, bulkFileName string, fileNames map[string]struct{}, outputPath string) {
-			if err := DownloadFiles(filesPath, bulkFileName, fileNames, outputPath); err != nil {
-				log.Println(err)
+		if nobulk {
+			for fileName, _ := range accumulatedImageNames {
+				sem <- struct{}{}
+				go func(filesPath, fileName string, outputPath string) {
+					if err := DownloadFilesNoBulk(filesPath, fileName, outputPath); err != nil {
+						log.Println(err)
+					}
+					<-sem
+				}(filesPath, fileName, outputPath)
 			}
-			<-sem
-		}(filesPath, lastBulk, accumulatedImageNames, outputPath)
+		} else {
+			sem <- struct{}{}
+			go func(filesPath, bulkFileName string, fileNames map[string]struct{}, outputPath string) {
+				if err := DownloadFiles(filesPath, bulkFileName, fileNames, outputPath); err != nil {
+					log.Println(err)
+				}
+				<-sem
+			}(filesPath, lastBulk, accumulatedImageNames, outputPath)
+		}
 	}
 
 	// Make sure we finish all the reads before we exit, otherwise we'll lose
@@ -163,6 +188,33 @@ func run() error {
 	return nil
 }
 
+func DownloadFilesNoBulk(filesPath, fileName string, outputPath string) error {
+	sourcePath := fmt.Sprintf("%s/%s", filesPath, fileName)
+
+	img, err := bulkprocess.MaybeExtractImageFromGoogleStorage(sourcePath, sclient)
+	if err != nil {
+		return err
+	}
+
+	// Save the images to disk
+	outFile := fmt.Sprintf("%s/%s", outputPath, fileName)
+	// log.Println(outFile)
+	f, err := os.Create(outFile)
+	if err != nil {
+		return err
+	}
+
+	// Write the PNG representation of our ID-encoded image to disk
+	if err := png.Encode(f, img); err != nil {
+		f.Close()
+		return err
+	}
+	f.Close()
+
+	return nil
+}
+
+// DownloadFiles pulls down files
 func DownloadFiles(filesPath, bulkFileName string, fileNames map[string]struct{}, outputPath string) error {
 	if !strings.HasSuffix(bulkFileName, ".tar.gz") {
 		return fmt.Errorf("currently only supports .tar.gz bulk files")
